05a1268aefc5aca3241268ffc462d3a82157ad3b
Ken Barlow <kenbrlow@gmail.com>
2021-03-08 12:10:37 -0700

    commit tree 6bb40115135d2aad5086a434d015b4119aeec9da

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,31 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    if birds
+      render json: @birds
+    else
+      render json: {message: 'Birds not found'}
+    end
+  end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+
+    if bird
+      # option 1
+      #render json: {id: bird.id, name: bird.name, species: bird.species }
+
+      # option 2
+      #render json: bird.slice(:id, :name, :species)
+
+      # option 3
+      # render json: bird, only: [:id, :name, :species]
+
+      # option 4
+      # exclude the keys instead of including keys
+      render json: bird, except: [:created_at, :updated_at]
+    else
+      render json: {message: 'Bird not found'}
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  
+
+  #api
   get '/birds' => 'birds#index'
+  get '/birds/:id' => 'birds#show' # new
 end
